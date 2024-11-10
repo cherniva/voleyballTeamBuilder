d5af4c5bbeb3944d3d412ee6560b08237463eb60
Ivan Chernikov <chernikoviivan@gmail.com>
2024-11-10 11:14:00 +0100

    Initial commit

---
+++ src/main/java/com/cherniva/voleyballteambuilder/VoleyballTeamBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.cherniva.voleyballteambuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoleyballTeamBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoleyballTeamBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cherniva/voleyballteambuilder/VoleyballTeamBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherniva.voleyballteambuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoleyballTeamBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
